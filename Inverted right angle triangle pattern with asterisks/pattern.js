41a0a7031bfe3eba87cd574d718fc9cb47ca4aa6
Pallavi Kumari <109738810+pallavik95@users.noreply.github.com>
2023-06-13 11:36:49 +0530

    Add files via upload

---
+++ Inverted right angle triangle pattern with asterisks/pattern.js
@@ -0,0 +1,11 @@
+let charstr = "";
+for(let i=6;i>0;i--){
+    for(let j=i;j>0;j--){
+        charstr +="*";
+       
+    }
+    // console.log("\n");
+    charstr+="\n";
+    
+}
+console.log(charstr);
